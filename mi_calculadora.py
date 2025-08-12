db9de425b3f48971ab34b70c8ec95329c3e4b1dd
🔹Esteban <esteban.horacio.acevedo@gmail.com>
2025-08-12 19:30:21 -0300

    feat: agrega función de multiplicación

--- mi_calculadora.py
+++ mi_calculadora.py
@@ -6,6 +6,10 @@ def restar(a, b):
     return a - b
 
 
+def multiplicar(a, b):
+    return a * b
+
+
 print("¡Bienvenido a mi calculadora!")
 
 resultado_suma = sumar(10, 5)
@@ -13,3 +17,6 @@ print(f"El resultado de la suma es: {resultado_suma}")
 
 resultado_resta = restar(10, 5)
 print(f"El resultado de la resta es: {resultado_resta}")
+
+resultado_multiplicacion = multiplicar(10, 5)
+print(f"El resultado de la multiplicación es: {resultado_multiplicacion}")

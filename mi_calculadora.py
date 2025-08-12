6b58ae6008cef51f5ecb576a0c237a59327ed494
🔹Esteban <esteban.horacio.acevedo@gmail.com>
2025-08-12 19:19:10 -0300

    feat: agrega función de resta

--- mi_calculadora.py
+++ mi_calculadora.py
@@ -2,6 +2,14 @@ def sumar(a, b):
     return a + b
 
 
+def restar(a, b):
+    return a - b
+
+
 print("¡Bienvenido a mi calculadora!")
+
 resultado_suma = sumar(10, 5)
 print(f"El resultado de la suma es: {resultado_suma}")
+
+resultado_resta = restar(10, 5)
+print(f"El resultado de la resta es: {resultado_resta}")
